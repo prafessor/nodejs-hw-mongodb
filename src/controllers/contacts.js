b5b444972a19f795f6b4faa374f156a810f818b2
prafessor <olegmilyo@gmail.com>
2025-05-29 00:02:37 +0300

    refactor getContactsController response

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -29,7 +29,10 @@ export const getContactsController = async (req, res) => {
   res.status(200).json({
     status: 200,
     message: 'Succesfully found contacts!',
-    data: contacts,
+    data: {
+      data: contacts.contacts,
+      ...contacts.paginationData
+    }
   });
 };
 
